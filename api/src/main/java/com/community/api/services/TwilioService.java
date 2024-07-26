fa0d3259b51fd2215a07e2f44ad0527b686cac51
Tarun Sharma <tarun.sharma@avisoft.io>
2024-07-26 13:25:56 +0530

    changes in account and customer endpoints -resolved some errors

--- api/src/main/java/com/community/api/services/TwilioService.java
+++ api/src/main/java/com/community/api/services/TwilioService.java
@@ -61,12 +61,12 @@ public class TwilioService {
 
             System.out.println(completeMobileNumber + " completeMobileNumber");
 
-           Message message = Message.creator(
+         /*  Message message = Message.creator(
                             new PhoneNumber(completeMobileNumber),
                             new PhoneNumber(twilioPhoneNumber),
                             otp)
 
-                    .create();
+                    .create();*/
 
 
             CustomCustomer existingCustomer = customCustomerService.findCustomCustomerByPhone(mobileNumber,countryCode);
