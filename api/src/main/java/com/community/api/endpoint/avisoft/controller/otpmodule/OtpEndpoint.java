dfdcaa6b75bc7a7d45930f77c52c378c178125a7
kshtjvats <Kshitij.vats@avisoft.io>
2024-08-26 12:29:47 +0530

    changes in web annotations

--- api/src/main/java/com/community/api/endpoint/avisoft/controller/otpmodule/OtpEndpoint.java
+++ api/src/main/java/com/community/api/endpoint/avisoft/controller/otpmodule/OtpEndpoint.java
@@ -23,7 +23,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.HttpClientErrorException;
 
 import javax.persistence.EntityManager;
