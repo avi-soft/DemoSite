993502e7dd144685b789380b2ada4032e2f7d22c
Kshitij Vats <158016609+kshtjvats@users.noreply.github.com>
2024-10-10 12:46:25 +0530

    Delete api/src/main/java/com/community/api/endpoint/avisoft/controller/order/OrderController.java

--- api/src/main/java/com/community/api/endpoint/avisoft/controller/order/OrderController.java
+++
@@ -1,9 +0,0 @@
-package com.community.api.endpoint.avisoft.controller.order;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class OrderController
-{
-    
-}
